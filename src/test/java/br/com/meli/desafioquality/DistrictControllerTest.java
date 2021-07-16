2acb0e7815404f928737dea7bbc2ceeeffcb6264
Andre Carlos Agostinho <andre.agostinho@mercadolivre.com>
2021-07-16 11:09:36 -0300

    refactor: create district test

--- src/test/java/br/com/meli/desafioquality/DistrictControllerTest.java
+++ src/test/java/br/com/meli/desafioquality/DistrictControllerTest.java
@@ -5,7 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,8 +17,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.math.BigDecimal;
 
-@ExtendWith(SpringExtension.class)
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class DistrictControllerTest {
 
     @Autowired
