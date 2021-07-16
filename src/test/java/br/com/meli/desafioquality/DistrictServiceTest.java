5ee240b3c0f9575b068b03463bd2d4492403aea9
Andre Carlos Agostinho <andre.agostinho@mercadolivre.com>
2021-07-16 12:11:49 -0300

    refactor: should not create property if district not exists

--- src/test/java/br/com/meli/desafioquality/DistrictServiceTest.java
+++ src/test/java/br/com/meli/desafioquality/DistrictServiceTest.java
@@ -4,15 +4,14 @@ import br.com.meli.desafioquality.exceptions.DistrictNotFoundException;
 import br.com.meli.desafioquality.services.DistrictService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class DistrictServiceTest {
 
-    @Mock
+    @Autowired
     private DistrictService districtService;
 
     public void init() {
@@ -21,8 +20,6 @@ public class DistrictServiceTest {
 
     @Test
     public void shouldNotCreatePropertyIfDistrictNotExists() {
-        Mockito.when(districtService.findById(1)).thenThrow(new DistrictNotFoundException());
-
         Assertions.assertThrows(DistrictNotFoundException.class, () -> {
             districtService.findById(1);
         });
