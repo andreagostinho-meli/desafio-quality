496549bcba25e874a8843c400a820777489cc1c2
Andre Carlos Agostinho <andre.agostinho@mercadolivre.com>
2021-07-16 16:09:28 -0300

    chore: remove unused mock initialization

--- src/test/java/br/com/meli/desafioquality/DistrictServiceTest.java
+++ src/test/java/br/com/meli/desafioquality/DistrictServiceTest.java
@@ -4,17 +4,12 @@ import br.com.meli.desafioquality.exceptions.DistrictNotFoundException;
 import br.com.meli.desafioquality.services.DistrictService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class DistrictServiceTest {
 
     @Autowired
     private DistrictService districtService;
-
-    public void init() {
-        MockitoAnnotations.openMocks(this);
-    }
 
     @Test
     public void shouldNotCreatePropertyIfDistrictNotExists() {
