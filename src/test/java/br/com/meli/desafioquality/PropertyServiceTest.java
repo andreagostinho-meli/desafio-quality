f155987b5a63204609953df8d882ac0c535f9f47
Andre Carlos Agostinho <andre.agostinho@mercadolivre.com>
2021-07-19 12:12:51 -0300

    refactor: property service test

--- src/test/java/br/com/meli/desafioquality/PropertyServiceTest.java
+++ src/test/java/br/com/meli/desafioquality/PropertyServiceTest.java
@@ -7,7 +7,12 @@ import br.com.meli.desafioquality.repositories.DistrictRepository;
 import br.com.meli.desafioquality.repositories.PropertyRepository;
 import br.com.meli.desafioquality.services.PropertyService;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -18,27 +23,29 @@ import java.util.List;
 @SpringBootTest
 public class PropertyServiceTest {
 
-    @Autowired
+    @InjectMocks
     private PropertyService propertyService;
 
-    @Autowired
+    @Mock
     private PropertyRepository propertyRepository;
 
-    @Autowired
+    @Mock
     private DistrictRepository districtRepository;
+
 
     @Test
     public void shouldCalculatePropertyTotalM2() {
         // arrange
-        districtRepository.create(new District(1, "Laranjeiras", new BigDecimal(400)));
-
         Room room1 = new Room("Sala de Estar", 20, 20);
         Room room2 = new Room("Cozinha", 30, 30);
 
-        propertyRepository.create(new Property(1, "Fazenda Boa Vista", 1, Arrays.asList(room1, room2)));
+        Property property = new Property(1, "Fazenda Boa Vista", 1, Arrays.asList(room1, room2));
+
+        Mockito.when(propertyRepository.findById(1)).thenReturn(property);
 
         // act
         double totalM2 = propertyService.calculatePropertyTotalM2(1);
+        Mockito.verify(propertyRepository).findById(1);
 
         // assert
         Assertions.assertEquals(1300, totalM2);
@@ -47,22 +54,23 @@ public class PropertyServiceTest {
     @Test
     public void shouldCalculatePropertyValue() {
         // arrange
-        District district = districtRepository.create(new District(1, "Laranjeiras", new BigDecimal(400)));
+        District district = new District(1, "Laranjeiras", new BigDecimal(400));
 
         Room room1 = new Room("Sala de Estar", 20, 20);
         Room room2 = new Room("Cozinha", 30, 30);
 
-        Property property = propertyRepository.create(
-            new Property(
-                1,
-                "Fazenda Boa Vista",
-                1,
-                Arrays.asList(room1, room2)
-            )
+        Property property = new Property(
+            1,
+            "Fazenda Boa Vista",
+            1,
+            Arrays.asList(room1, room2)
         );
+
+        Mockito.when(propertyRepository.findById(1)).thenReturn(property);
 
         // act
         BigDecimal propertyValue = propertyService.calculatePropertyValue(property, district);
+        Mockito.verify(propertyRepository).findById(1);
 
         // assert
         Assertions.assertEquals(BigDecimal.valueOf(520000.0), propertyValue);
@@ -71,22 +79,21 @@ public class PropertyServiceTest {
     @Test
     public void shouldFindLargestRoom() {
         // arrange
-        District district = districtRepository.create(new District(1, "Laranjeiras", new BigDecimal(400)));
-
         Room room1 = new Room("Sala de Estar", 20, 20);
         Room room2 = new Room("Cozinha", 30, 30);
 
-        Property property = propertyRepository.create(
-            new Property(
-                1,
-                "Fazenda Boa Vista",
-                1,
-                Arrays.asList(room1, room2)
-            )
+        Property property = new Property(
+            1,
+            "Fazenda Boa Vista",
+            1,
+            Arrays.asList(room1, room2)
         );
+
+        Mockito.when(propertyRepository.findById(1)).thenReturn(property);
 
         // act
         Room largestRoom = propertyService.findLargestRoom(property.getId());
+        Mockito.verify(propertyRepository).findById(1);
 
         // assert
         Assertions.assertEquals(room2, largestRoom);
@@ -95,22 +102,21 @@ public class PropertyServiceTest {
     @Test
     public void shouldCalculateM2ForAllRooms() {
         // arrange
-        District district = districtRepository.create(new District(1, "Laranjeiras", new BigDecimal(400)));
-
         Room room1 = new Room("Sala de Estar", 20, 20);
         Room room2 = new Room("Cozinha", 30, 30);
 
-        Property property = propertyRepository.create(
-                new Property(
-                        1,
-                        "Fazenda Boa Vista",
-                        1,
-                        Arrays.asList(room1, room2)
-                )
+        Property property = new Property(
+            1,
+            "Fazenda Boa Vista",
+            1,
+            Arrays.asList(room1, room2)
         );
+
+        Mockito.when(propertyRepository.findById(1)).thenReturn(property);
 
         // act
         List<Double> m2Values = propertyService.calculateM2ForAllRooms(property.getId());
+        Mockito.verify(propertyRepository).findById(1);
 
         // assert
         Assertions.assertEquals(400, m2Values.get(0));
